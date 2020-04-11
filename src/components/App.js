8b5060e8947e517aba499f3450bb720b39510984
Michael Zadra <michaelzadra1@gmail.com>
2020-04-11 15:23:57 -0400

    Adds a CallToAction component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,17 @@
 import 'typeface-roboto';
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
 import { AuthProvider } from '../context/Auth';
 import Header from './Header';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import CallToAction from './CallToAction';
 
 const App = () => {
 	return (
 		<AuthProvider>
 			<CssBaseline />
 			<Header />
+			<CallToAction />
 		</AuthProvider>
 	);
 };
---
+++ src/components/CallToAction.js
@@ -0,0 +1,41 @@
+import React, { useContext } from 'react';
+import { Container, Typography, Box } from '@material-ui/core';
+
+import { AuthContext } from '../context/Auth';
+
+const CallToAction = () => {
+	const d = new Date();
+	const dayOfTheWeek = d.toLocaleString('en-us', {
+		weekday: 'long'
+	});
+
+	const { isSignedIn } = useContext(AuthContext);
+
+	return (
+		<Container maxWidth="sm">
+			<Box my={3}>
+				<Typography
+					component="h1"
+					variant="h2"
+					align="center"
+					color="textPrimary"
+					gutterBottom
+				>
+					Welcome to To Do
+				</Typography>
+				<Typography
+					component="h2"
+					variant="h5"
+					align="center"
+					color="textSecondary"
+				>
+					{`Happy ${dayOfTheWeek}! ${
+						isSignedIn ? "Manage your To Do's below" : 'Please sign In'
+					}`}
+				</Typography>
+			</Box>
+		</Container>
+	);
+};
+
+export default CallToAction;
